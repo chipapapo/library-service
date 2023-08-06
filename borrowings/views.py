023305759aac0afa67c9b269785e3f13b99fa27c
chipa <mezismezis@gmail.com>
2023-08-06 14:12:44 +0300

    viewset for borrowing

---
+++ borrowings/views.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from rest_framework import viewsets
+
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer
+from user.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly, )
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.request.user.is_staff:
+            date = self.request.query_params.get("date")
+            user_id_str = self.request.query_params.get("user")
+
+            if date:
+                date = datetime.strptime(date, "%Y-%m-%d").date()
+                queryset = queryset.filter(show_time__date=date)
+
+            if user_id_str:
+                queryset = queryset.filter(user_id=int(user_id_str))
+
+            return queryset
+
+        return queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BorrowingListSerializer
+
+        return BorrowingSerializer
